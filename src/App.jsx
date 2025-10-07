61d34c9072f97736aad7181af2b8f6fb1de09d10
tlshudson <hudsonteles00@gmail.com>
2025-10-07 00:49:14 -0300

    feat(FT-004): Add function completed tasks

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,25 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  function onTaskClick(taskId) {
+    const newTask = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+    setTasks(newTask);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
         <AddTask />
       </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,18 @@
+import { ChevronRightIcon, Trash } from "lucide-react";
+
 function Tasks(props) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
-        <li key={task.id} className="flex">
-          <button className="bg-slate-400 w-full text-white p-2 rounded-md">
+        <li key={task.id} className="flex gap-2">
+          <button onClick={() => props.onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white p-2 ${task.isCompleted && "line-through"}`}>
             {task.title}
           </button>
-          <button className="">
-            Ver Detalhes
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <Trash />
           </button>
         </li>
       ))}
