45a74a36f6ed57807047d1029212d3cbf493b1ac
tlshudson <hudsonteles00@gmail.com>
2025-10-06 23:58:37 -0300

    feat(FT-002): Add Tailwind, Add details of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Configurar Tailwind CSS no projeto",
+      description:
+        "Revisar 'index.css' e 'tailwind.config.js' para garantir que todas as classes utilitárias sejam carregadas corretamente.",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      title: "Criar componente 'TaskItem'",
+      description:
+        "Desenvolver o componente JSX para renderizar um único item da lista, incluindo título, descrição e um checkbox.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Implementar a funcionalidade de 'Adicionar'",
+      description:
+        "Ligar o input do 'AddTask' a um estado e criar a função para adicionar um novo objeto de tarefa (com um ID único) ao array principal de tarefas.",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      title: "Estilizar o Gerenciador com classes do Tailwind",
+      description:
+        "Aplicar classes de flexbox (flex), cores de fundo (bg-), e responsividade para dar um visual moderno à aplicação.",
+      isCompleted: false,
+    },
+    {
+      id: 5,
+      title: "Adicionar botão 'Deletar' em cada tarefa",
+      description:
+        "Implementar a função de filtro para remover uma tarefa do estado ao clicar no botão 'Deletar'.",
+      isCompleted: false,
+    },
+  ]);
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks tasks={tasks}/>
+        <AddTask />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,18 @@
-function Tasks() {
-    return (
-        <h1>Tarefas</h1>
-    )
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex">
+          <button className="bg-slate-400 w-full text-white p-2 rounded-md">
+            {task.title}
+          </button>
+          <button className="">
+            Ver Detalhes
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
